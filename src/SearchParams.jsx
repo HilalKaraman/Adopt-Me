3d22932e69461e11f443595de880fffa549a3d58
Hilal Karaman <02180201039@ogr.inonu.edu.tr>
2024-11-21 17:02:01 +0300

    code result

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,13 +1,14 @@
 import { useState, useEffect } from "react";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
-import Pet from "./Pet";
+import useBreedList from "./useBreedList";
+import Results from "./Result";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
   const [pets, setPets] = useState([]);
-  const breeds = [];
+  const [breeds] = useBreedList(animal);
 
   useEffect(() => {
     requestPets();
@@ -77,14 +78,7 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
-      {pets.map((pet) => (
-        <Pet
-          name={pet.name}
-          animal={pet.animal}
-          breed={pet.breed}
-          key={pet.id}
-        />
-      ))}
+      <Results pets={pets} />
     </div>
   );
 };
